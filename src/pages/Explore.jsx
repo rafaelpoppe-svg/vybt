922f647a5d2f151093ea1afa634323e4caa16d18
vallyt <vallyt@vallyt.com>
2026-02-21 19:14:30 +0000

    File changes

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -357,6 +357,7 @@ export default function Explore() {
             allParticipants={allParticipants}
             profilesMap={profilesMap}
             myParticipations={myParticipations}
+            selectedCity={selectedCity}
           />
         </div>
       )}
