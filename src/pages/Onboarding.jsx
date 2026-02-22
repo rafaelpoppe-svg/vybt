65ff9823b207c4dcdc82d2d45aa28528846b6c9b
vallyt <vallyt@vallyt.com>
2026-02-22 13:16:44 +0000

    File changes

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -258,7 +258,7 @@ function OnboardingInner() {
       </div>
 
       {/* Footer Button */}
-      {step < 7 && (
+      {step < 8 && (
         <div className="fixed bottom-0 left-0 right-0 p-6 bg-gradient-to-t from-[#0b0b0b] via-[#0b0b0b] to-transparent">
           <motion.button
             whileHover={{ scale: canProceed() ? 1.02 : 1 }}
