c41b532321ef9f1e90a5063420064660b973307c
vallyt <vallyt@vallyt.com>
2026-03-17 16:49:14 +0000

    File changes

--- src/pages/CommunityView.jsx
+++ src/pages/CommunityView.jsx
@@ -17,6 +17,7 @@ import CommunityActivityFeed from '../components/community/CommunityActivityFeed
 import CommunityNewMemberGuide from '../components/community/CommunityNewMemberGuide';
 import CommunityChallengeBanner from '../components/community/CommunityChallengeBanner';
 import CommunityCreateChallengeModal from '../components/community/CommunityCreateChallengeModal';
+import CommunityChallengeDetail from '../components/community/CommunityChallengeDetail';
 
 const TABS = [
   { key: 'plans', label: 'Plans', icon: <CalendarDays className="w-4 h-4" /> },
@@ -506,17 +507,6 @@ export default function CommunityView() {
           <CommunityNewMemberGuide
             communityId={communityId}
             onClose={() => setShowNewMemberGuide(false)}
-          />
-        )}
-      </AnimatePresence>
-
-      <AnimatePresence>
-        {showCreateChallenge && (
-          <CommunityCreateChallengeModal
-            communityId={communityId}
-            plans={plans}
-            tc={tc}
-            onClose={() => setShowCreateChallenge(false)}
           />
         )}
       </AnimatePresence>
