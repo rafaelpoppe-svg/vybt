fd225924f7dee095032d7de1f3100e3bf8479bdf
vallyt <vallyt@vallyt.com>
2026-03-20 17:06:27 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/AddStory" element={<AddStory />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
